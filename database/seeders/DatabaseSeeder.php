6a294a537da6015f8ad914badabc341a71f1e918
steeven-js <kisama@gmail.com>
2023-12-19 15:01:08 -0400

    Add SymptomePlantSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PlantSeeder::class,
             SymptomeSeeder::class,
+            SymptomePlantSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/SymptomePlantSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Symptome;
+use App\Models\Plant;
+use App\Models\SymptomePlant;
+
+class SymptomePlantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Retrieve all Symptome and Plant records
+        $symptomes = Symptome::all();
+        $plants = Plant::all();
+
+        // Seed SymptomePlant table
+        foreach ($plants as $plant) {
+            // Get a random number of symptoms to associate with each plant
+            $randomSymptoms = $symptomes->random(rand(1, $symptomes->count()));
+
+            // Associate each random symptom with the current plant
+            foreach ($randomSymptoms as $symptome) {
+                SymptomePlant::create([
+                    'symptome_id' => $symptome->id,
+                    'plant_id' => $plant->id,
+                ]);
+            }
+        }
+    }
+}

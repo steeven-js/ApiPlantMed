2b5a0e6b0456e32056e304b1cfdd48d689b77e8b
steeven-js <jacques@gmail.com>
2024-02-12 18:09:50 -0400

    Ajouter la fonctionnalité de collapsible sidebar sur desktop dans AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
                 'success' => Color::Emerald,
                 'warning' => Color::Orange,
             ])
+            ->sidebarCollapsibleOnDesktop()
             ->favicon(asset('images/favicon.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')

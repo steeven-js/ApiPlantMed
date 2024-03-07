5bab81d6c08fb9ad50a5a8498f6981fde49cc7e3
steeven-js <jacques.steeven@gmail.com>
2024-03-07 01:47:25 -0400

    Mise à jour de la récupération des médias pour la plante

--- app/Http/Controllers/API/PlantController.php
+++ app/Http/Controllers/API/PlantController.php
@@ -22,7 +22,7 @@ class PlantController extends Controller
     public function show(Plant $plant)
     {
         // Je récupère le plant avec ses informations
-        $plant = Plant::with('proprietes', 'precautions', 'utilisations', 'precautions', 'media')
+        $plant = Plant::with('media')
         ->where('isActive', 1)
         ->find($plant->id);
 

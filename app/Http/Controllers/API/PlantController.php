e9081ad61fe606232f5c8ef414390f43e1c9bc75
steeven-js <jacques.steeven@gmail.com>
2024-03-07 04:49:29 -0400

    Mise à jour du contrôleur PlantController.php

--- app/Http/Controllers/API/PlantController.php
+++ app/Http/Controllers/API/PlantController.php
@@ -15,23 +15,18 @@ class PlantController extends Controller
         ->where('isActive', 1)
         ->get();
 
-        dd($plants);
-
         // On retourne les informations des plants en JSON
         return response()->json($plants);
     }
 
-    public function show(Symptome $symptome)
+    public function show(Plant $plant)
     {
-        // Eager load the 'plants' relationship
-        $symptome = Symptome::with('plants')->where('isActive', 1)->find($symptome->id);
+        // Je récupère le plant avec ses informations
+        $plant = Plant::with('media')
+        ->where('isActive', 1)
+        ->find($plant->id);
 
-        // Check if $symptome is null, meaning the Symptome was not found
-        if (!$symptome) {
-            return response()->json(['message' => 'Symptome not found'], 404);
-        }
-
-        return response()->json($symptome);
+        // Je retourne le plant en JSON
+        return response()->json($plant);
     }
-
 }
